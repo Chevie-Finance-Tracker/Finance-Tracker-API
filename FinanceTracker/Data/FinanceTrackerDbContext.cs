48d438a12c59352a8e939a2351b0988051b2361d
Chevie Rae Jadraque <179885376+chevie22@users.noreply.github.com>
2026-01-06 15:12:52 +0800

    Initialize DbContext

---
+++ FinanceTracker/Data/FinanceTrackerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FinanceTracker.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTracker.Data
+{
+    public class FinanceTrackerDbContext : DbContext
+    {
+        public FinanceTrackerDbContext(DbContextOptions<FinanceTrackerDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Spending> Spendings { get; set; }
+    }
+}
