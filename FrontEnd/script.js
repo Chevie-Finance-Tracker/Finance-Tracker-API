c9004176d7ea8939709143077dd62cba8ad604dd
Yman <64341841+Yman14@users.noreply.github.com>
2026-01-08 17:43:14 +0800

    fetch data

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,4 +4,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const messageDiv = document.getElementById('message');
+
+     // The specific API endpoint from your running C# backend
+    const API_URL = 'http://localhost:5018/register';
 });
